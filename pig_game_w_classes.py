0e7d1177891674088ec10d38d19f01db346cd1c9
Yuriy Drubinskiy <ydrubins@yahoo.com>
2023-07-08 13:54:31 -0500

    Input checking and syntax cleanup

--- pig_game_w_classes.py
+++ pig_game_w_classes.py
@@ -29,19 +29,6 @@ class Turn:
             return self.roll_score, 'cont'
 
 
-    def score_calc(self, roll_result, turn_result):
-        # print(roll_result)
-        if roll_result > 0:
-            self.round_score += roll_result
-            self.total_score += roll_result
-            return self.round_score, self.total_score
-
-        elif roll_result == 0 and turn_result == 'lose_turn':
-            self.round_score = 0
-
-        elif roll_result == 0 and turn_result == 'lose_points':
-            pass
-
     def Turn(self, human_turn = True): #True = player control, false = Computer control
         turn_result =  self.roll_result()
         print(turn_result)
@@ -51,53 +38,86 @@ class Turn:
             print(f'Your current round score is: {self.round_score}\n'
                   f'Your current total score is: {self.total_score} ')
 
-            if human_turn == True:
+            if human_turn:
                 again = self.player_input()
                 if again == 'y':
                     print('you have decided to roll again')
                     print()
                     self.Turn()
 
-                if again == 'n':
+                elif again == 'n':
                     print('you have ended your turn')
+
+            else:
+                return turn_result
 
         if turn_result[1] == 'lose_turn':
             print('you rolled a six and lost your turn as well as points for this round.')
             self.total_score -= self.round_score
             print(f'Your current total score is: {self.total_score}')
-            return self.total_score
+            return self.total_score, turn_result
 
         if turn_result[1] == 'lose_points':
             print('you rolled TWO six and lost ALL your points.')
             self.total_score = 0
-            return self.total_score
+            return self.total_score, turn_result
 
         # print(f'Your current round score is: {self.round_score}\n'
         #       f'Your current total score is: {self.total_score} ')
         self.round_score = 0
 
     def player_input(self):
-        again =  input("Roll again(Y/N)?: ").casefold()
-        print()
-        return again
+        while True:
+            again =  input("Roll again(Y/N)?: ").casefold()
+            if again == 'y' or again == 'n':
+                print()
+                return again
+            print("invalid input")
+            print()
+
+    def computer_strategy1(self):
+        self.round_score = 0
+        re_roll = random.randint(1,3)
+        for i in range(re_roll):
+            result = self.Turn(False)
+            # print(result[1][1])
+            if result[1][1] == 'lose_turn':
+                break
+            if result[1][1] == 'lose_points':
+                break
 
     def score_data(self):
         return self.total_score
 
+# def score_card(score1, score2, round):
+    # top_bottom = ' ' * 9 + '*' * 24
+    # score_row = '\n' + 'round #' + str(round) + ' ' + '*' + ' ' * 4 + str(score1) + ' ' * 4 + '*' + ' ' * 4 + str(score2) + ' ' * 4 + '*'
+    # print ((top_bottom + score_row + '\n')* round + top_bottom)
+
+scores = []
+def score_card(score1, score2, num_rounds):
+    top_bottom = ' ' * 9 + '*' * 24
+    score_rows = ''
+    scores.append((score1, score2))
+
+    for round_num in range(1, num_rounds + 1):
+        score_row = '\n' + 'round #' + str(round_num) + ' ' + '*' + ' ' * 4 + str(
+            scores[round_num -1][0]) + ' ' * 4 + '*' + ' ' * 4 + str(scores[round_num -1][1]) + ' ' * 4 + '*' + '\n'
+        score_rows += top_bottom + score_row
+
+    print('\n' + score_rows + top_bottom)
+    print(scores)
 
 
 player = Turn()
 computer = Turn()
 total_score = 0
 
-for i in range (2):
-    print('*' * 20, 'Players Turn', '*' *20)
+for i in range (5):
+    print('*' * 20, 'Players Turn', '*' * 20)
     player.Turn(True)
-    print('*' * 20, 'Computers Turn', '*' *20)
-    computer.Turn(False)
+    print('*' * 20, 'Computers Turn', '*' * 20)
+    computer.computer_strategy1()
+    score_card(player.score_data(), computer.score_data(), i+1)
 
-print('\n', '*' * 20, 'RESULTS', '*' *20)
-
-print(f'Players Final Score: {player.score_data()}')
-print(f'Computers Final Score: {computer.score_data()}')
-
+# computer.computer_strategy1()
